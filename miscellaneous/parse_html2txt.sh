7de75048bcfbc4a8bf5c25d8948877fe12387664
Carlos Santa <santa.cm@gmail.com>
2018-11-12 07:24:09 -0800

    miscel: revamp the parser
    	- parse now email + language
    	- improve villa keyword naming
    
    Signed-off-by: Carlos Santa <santa.cm@gmail.com>

--- miscellaneous/parse_html2txt.sh
+++ miscellaneous/parse_html2txt.sh
@@ -3,40 +3,51 @@
 FILE=$1
 guestname=""
 phone=""
+email=""
 checkin=""
 checkout=""
 totalguests=""
 checkin_count=0
 checkout_count=0
 villa=0
-outputfile=""
+language=""
+outputfile=${1%.txt}.out
 
 while read LINE; do
 	oneline=$LINE
 	#Look for Villa Name
-	if [ `echo $oneline | grep -c "Rustica" ` -gt 0 ]
+	if [ `echo $oneline | grep -c "Rustica Beach House Property" ` -gt 0 ]
 	then
 		villa=830
-	elif [ `echo $oneline | grep -c "Tortuga" ` -gt 0 ]
+		echo $oneline
+	elif [ `echo $oneline | grep -c "Tortuga Beach House Property" ` -gt 0 ]
 	then
 		villa=826
-	elif [ `echo $oneline | grep -c "Acqualina" ` -gt 0 ]
+	elif [ `echo $oneline | grep -c "Acqualina Beach House Property" ` -gt 0 ]
 	then
 		villa=778
-	elif [ `echo $oneline | grep -c "Solana" ` -gt 0 ]
+	elif [ `echo $oneline | grep -c "Solana Beach House Property" ` -gt 0 ]
 	then
 		villa=752
+		echo $oneline
 	#Look for Guest Name only in the line
 	elif [ `echo $oneline | grep -c "Guest Name" ` -gt 0 ]
 	then
-		guestname=`echo $oneline | grep -Eo '[A-Za-z]'|tr '\n' '\0'`
-		guestname=${guestname##GuestName}
-		outputfile=$guestname.txt
+		guestname=`echo $oneline | grep -Eo '[A-Za-zé]+'| tr '\n' '-\0'`
+		guestname=${guestname##Guest-Name-}
+	elif [ `echo $oneline | grep -c "Preferred language" ` -gt 0 ]
+	then
+		language=`echo $oneline | grep -Eo '[A-Za-z]+'| tr '\n' '-\0'`
+		language=${language##Preferred-language-}
 	#Phone Number
 	elif [ `echo $oneline | grep -c "phone"` -gt 0 ]
 	then
 		#parse only numerals
 		phone=`echo $oneline | grep -Eo '[0-9]{1,10}' | tr '\n' '\0'`
+	#Email alias
+	elif [ `echo $oneline | grep -c "@"` -gt 0 ]
+	then
+		email=$oneline
 	#Total # of guests
 	elif [ `echo $oneline | grep -c "Total guests" ` -gt 0 ]
 	then
@@ -66,12 +77,17 @@ while read LINE; do
 
 done < $FILE
 
+	#echo "\nGuest Record from filename: $FILE"
+	echo $villa
 	echo "$villa" | tee -a $outputfile
 	echo "$guestname" | tee -a $outputfile
 	echo "${phone}" | tee -a $outputfile
 	echo "$totalguests"  | tee -a $outputfile
 	echo "$checkin" | tee -a $outputfile
 	echo "$checkout" | tee -a $outputfile
+	echo "$email"  | tee -a $outputfile
+	echo "$language" | tee -a $outputfile
+	echo -n "" > /dev/null
 
 
 
